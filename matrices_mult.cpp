51e84a2380093f6de6fb41e875e291be425c0bd7
jisantamariar1 <jisantamariar1@uce.edu.ec>
2026-06-09 08:10:01 -0500

    resultado multiplicacion

--- matrices_mult.cpp
+++ matrices_mult.cpp
@@ -22,6 +22,26 @@ void imprimir_matriz(const std::vector<double> &A, int rows, int cols)
         fmt::println("");
     }
 }
+void imprimir_vector(const std::vector<double> &v)
+{
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        fmt::print("{:.2f} ", v[i]);
+    }
+    fmt::println("");
+}
+void multiplicar_matriz_vector(const std::vector<double> &A, const std::vector<double> &b, std::vector<double> &x, int rows, int cols)
+{
+    for (int i = 0; i < rows; ++i){
+        double sum = 0.0;
+        //x[i] = 0.0; // Inicializa el resultado para la fila i
+        for (int j = 0; j < cols; ++j){
+            int index = i * cols + j; // Convierte (i,j) a índice lineal
+            sum += A[index] * b[j]; // Multiplica y acumula
+        }
+        x[i] = sum; // Asigna el resultado final a x[i]
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -74,7 +94,7 @@ int main(int argc, char **argv)
         // Inicializa el vector b
         for (int i = 0; i < MATRIX_DIM; i++)
         {
-            b[i] = i;
+            b[i] = 1.0; // Llena b con 1.0 para simplificar
         }
 
         // Muestra información general de la distribución
@@ -124,7 +144,17 @@ int main(int argc, char **argv)
                 0,                                       // Tag
                 MPI_COMM_WORLD                          // Comunicador
             );
+            //enviar el vector b a todos los procesos trabajadores
+            MPI_Send(
+                b.data(),        // Buffer de envío
+                MATRIX_DIM,      // Cantidad de elementos
+                MPI_DOUBLE,      // Tipo de dato
+                i,               // Destino
+                0,               // Tag
+                MPI_COMM_WORLD   // Comunicador
+            );
         }
+        
 
         // Muestra la porción que teóricamente maneja el rank 0
         fmt::println(
@@ -132,11 +162,47 @@ int main(int argc, char **argv)
             rank,
             rows_per_rank,
             MATRIX_DIM);
+        // El proceso 0 también procesa su bloque de filas
+        std::vector<double> x_local(rows_per_rank);
+        fmt::println("Rank_{} resultado local:\n", rank);
+        multiplicar_matriz_vector(A, b, x, rows_per_rank, MATRIX_DIM);
+
+        imprimir_vector(x);
+        //recibe resultados locales de los procesos trabajadores y los combina en el vector solución x
+        for (int i=1;i<nproc;i++){
+            int filas = rows_per_rank;
+            if (i == nproc - 1)
+            {
+                filas = rows_per_rank - padding;
+            }
+            /*
+            [0,1,2,3,4,5,6,7,8,9,10,11,12...............,24]
+            R0[x x x x x x x, ........................]
+            R1[.............., y y y y y y y y y y y y................]
+            R2[.............., ....................., z z z z z z z z z z z z]
+            R3[.............., ....................., ....................., w w w w ] aqui van solo 4
+            */
+            MPI_Recv(
+                x.data() + i * rows_per_rank, // Buffer de recepción (coloca el resultado en la posición correcta de x)
+                filas,                 // Cantidad de elementos a recibir
+                MPI_DOUBLE,           // Tipo de dato
+                i,                    // Origen
+                0,                    // Tag
+                MPI_COMM_WORLD,       // Comunicador
+                MPI_STATUS_IGNORE     // Ignorar información de estado
+            );
+
+        }
+            fmt::println("Imprime resultado final en RANK_{}:\n", rank);
+            imprimir_vector(x);
+
     }
     else
     {
         // Vector para recibir los metadatos enviados por el rank 0
         std::vector<int> data_rec(2);
+        //b local
+        std::vector<double> b_local(MATRIX_DIM);
 
         // Recibe MATRIX_DIM y número de filas asignadas
         MPI_Recv(
@@ -175,9 +241,33 @@ int main(int argc, char **argv)
             MPI_COMM_WORLD,     // Comunicador
             MPI_STATUS_IGNORE   // Ignorar estado
         );
+        // Vector para recibir el vector b
+        MPI_Recv(
+            b_local.data(),     // Buffer donde se guardará el vector b
+            MATRIX_DIM,         // Cantidad de elementos a recibir
+            MPI_DOUBLE,         // Tipo de dato
+            0,                  // Proceso origen
+            0,                  // Tag
+            MPI_COMM_WORLD,     // Comunicador
+            MPI_STATUS_IGNORE   // Ignorar estado
+        );
+        //multiplicar la matriz local por el vector b local y almacenar el resultado en x_local ??? donde
+        std::vector<double> x_local(rows);
+        multiplicar_matriz_vector(A_local, b_local, x_local, rows, matrix_dim);
+        //enviar resultado local a rank 0
+        MPI_Send(
+            x_local.data(),     // Buffer de envío (puede ser el resultado local)
+            rows,               // Cantidad de elementos
+            MPI_DOUBLE,         // Tipo de dato
+            0,                  // Destino
+            0,                  // Tag
+            MPI_COMM_WORLD      // Comunicador
+        );
+        
 
-        // Sólo el proceso 1 imprime su bloque recibido
-        if (rank == 1)
+
+        // Sólo el proceso 2 imprime su bloque recibido
+        if (rank == 2)
         {
             fmt::println(
                 "Matriz local recibida por RANK_{}:\n",
@@ -188,8 +278,14 @@ int main(int argc, char **argv)
                 A_local,
                 rows,
                 matrix_dim);
+                fmt::println("");
+            fmt::println(
+                "Vector b local recibido por RANK_{}:\n",
+                rank);
+            imprimir_vector(b_local);
         }
     }
+
 
     // Finaliza el entorno MPI y libera recursos
     MPI_Finalize();
